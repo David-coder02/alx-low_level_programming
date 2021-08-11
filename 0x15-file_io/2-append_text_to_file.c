25febcda7557d53c051f95515dece26891051f8b
David-coder02 <davidmushi88@gmail.com>
2021-08-11 09:47:21 -0700

    made changes to file 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,25 +9,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-  int fd, t, s = 0;
+int fd, t, s = 0;
 
-  if (!filename)
-    return (-1);
+if (!filename)
+return (-1);
 
-  fd = open(filename, O_WRONLY | O_APPEND);
-  if (fd < 0)
-    return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd < 0)
+return (-1);
 
-  if (text_content)
-    {
-      while (text_content[s])
-	s++;
-      t = write(fd, text_content, s);
-      if (t != s)
-	return (-1);
-    }
+if (text_content)
+{
+while (text_content[s])
+s++;
+t = write(fd, text_content, s);
+if (t != s)
+return (-1);
+}
 
-  close(fd);
+close(fd);
 
-  return (1);
+return (1);
 }
